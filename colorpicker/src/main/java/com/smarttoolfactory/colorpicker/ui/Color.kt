c17f9cb7f204bb65e75aad817f1e5ac8d962259c
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-03-09 22:10:18 +0300

    Add gradient colors

---
+++ colorpicker/src/main/java/com/smarttoolfactory/colorpicker/ui/Color.kt
@@ -0,0 +1,23 @@
+package com.smarttoolfactory.colorpicker.ui
+
+import androidx.compose.ui.graphics.Color
+
+val gradientColors = listOf(
+    Color.Red,
+    Color.Magenta,
+    Color.Blue,
+    Color.Cyan,
+    Color.Green,
+    Color.Yellow,
+    Color.Red
+)
+
+val gradientColorsReversed = listOf(
+    Color.Red,
+    Color.Yellow,
+    Color.Green,
+    Color.Cyan,
+    Color.Blue,
+    Color.Magenta,
+    Color.Red
+)
